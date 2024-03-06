b826ba961e9d3634fc4a059f612ed903ff3dda28
StepanTrubcov <147098611+2010u@users.noreply.github.com>
2024-03-06 21:45:48 +0300

    Add files via upload

---
+++ 03.js
@@ -0,0 +1,11 @@
+let inputIn0 = document.querySelector(".input-in");
+
+let inputIn1 = document.querySelector("._input-in");
+
+let _myButton = document.getElementById('myButton');
+
+function myFunction() {
+  console.log(inputIn0.value);
+  console.log(inputIn1.value);
+  alert('Сообщение отправлено')
+}
